e1e276113ad8f4448bda1276102b382f06bf2f72
acgkaka <771679640@qq.com>
2023-01-27 18:06:32 +0800

    Add springboot-sharding-jdbc-month

--- springboot-sharding-jdbc-month/src/main/java/com/demo/module/entity/User.java
+++ springboot-sharding-jdbc-month/src/main/java/com/demo/module/entity/User.java
@@ -1,15 +1,15 @@
 package com.demo.module.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import java.io.Serializable;
-
-import lombok.AllArgsConstructor;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -36,26 +36,31 @@ public class User implements Serializable {
     /**
      * 用户名
      */
+    @TableField("username")
     private String username;
 
     /**
      * 密码
      */
+    @TableField("password")
     private String password;
 
     /**
      * 年龄
      */
+    @TableField("age")
     private Integer age;
 
     /**
      * 创建时间
      */
+    @TableField(value = "createTime", updateStrategy = FieldStrategy.NEVER)
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
+    @TableField("updateTime")
     private LocalDateTime updateTime;
 
     public User(String username, String password, Integer age, LocalDateTime createTime, LocalDateTime updateTime) {
