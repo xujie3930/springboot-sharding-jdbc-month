01139ec133be87403ef553c3f103ce9db682c843
ACGkaka <771679640@qq.com>
2023-03-22 09:38:01 +0800

    Add springboot-admin

---
+++ springboot-admin/src/main/java/com/demo/SpringbootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
